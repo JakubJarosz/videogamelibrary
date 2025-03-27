d3757a136ad1c6efc148e38f2954808c228f0b12
JakubJarosz <95305824+JakubJarosz@users.noreply.github.com>
2025-03-27 19:03:34 +0100

    ad

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,25 +1,45 @@
-import React, {useContext} from 'react'
-import {ThemeContext} from "../theme/ThemeContext"
-import { logoutUser } from '../state/authSlice'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import Button from "@mui/material/Button";
+import React, { useContext } from "react";
+import { ThemeContext } from "../theme/ThemeContext";
+import { logoutUser } from "../state/authSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { AppBar, Box, Button, Toolbar, IconButton, Typography } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 const Navbar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const {darkMode, toggleDarkMode} = useContext(ThemeContext)
+  const { darkMode, toggleDarkMode } = useContext(ThemeContext);
 
   const handleLogout = () => {
     dispatch(logoutUser());
-    navigate("/login")
-  }
+    navigate("/login");
+  };
   return (
     <div>
-      <button onClick={handleLogout}>Logout</button>
-      <Button  color="secondary" variant="contained" onClick={toggleDarkMode}>toggleDarkTheme {darkMode}</Button>
-    </div>
-  )
-}
+      {/* <button onClick={handleLogout}>Logout</button>
+      <Button color="secondary" variant="contained" onClick={toggleDarkMode}>
+        toggleDarkTheme {darkMode}
+      </Button>
+      <Box>
 
-export default Navbar
+      </Box> */}
+      <Box>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton>
+              <MenuIcon/>
+            </IconButton>
+            <Typography>
+              Menu
+            </Typography>
+            <Button>Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
+  );
+};
+
+export default Navbar;
