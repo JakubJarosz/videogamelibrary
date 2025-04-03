65434713e0c9ce47a21761cdd402df4a029e6082
JakubJarosz <95305824+JakubJarosz@users.noreply.github.com>
2025-04-03 17:02:11 +0200

    ad

--- client/src/components/CustomModal.jsx
+++ client/src/components/CustomModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import {
@@ -7,18 +7,33 @@ import {
   Backdrop,
   Fade,
   Typography,
+  TextField,
+  Button,
+  Avatar,
 } from "@mui/material";
 
 const CustomModal = ({ openModal, handleCloseModal }) => {
   const userId = useSelector((state) => state.auth.user?._id);
-  const steamId = "76561198121520859";
+  const [steamId, setSteamId] = useState("");
+  const [steamData, setSteamData] = useState({});
+  const [error, setError] = useState(false);
 
-  const handleClick = async () => {
+  const handleChange = (e) => {
+    const {value} = e.target;
+    setSteamId(value)
+    if (value) setError(false)
+  }
+
+  const handlefetchSteam = async () => {
     try {
       const response = await axios.get("/steamProfile", {
         params: { steamId },
       });
-      console.log(response.data);
+      if (response.data) {
+        setSteamData(response.data);
+      } else {
+        setError(true);
+      }
     } catch (error) {}
   };
 
@@ -30,49 +45,62 @@ const CustomModal = ({ openModal, handleCloseModal }) => {
       console.log("ERROR", error);
     }
   };
+
   return (
-    <div>
-      <MUIModal
-        aria-labelledby="spring-modal-title"
-        aria-describedby="spring-modal-description"
-        open={openModal}
-        onClose={handleCloseModal}
-        closeAfterTransition
-        slots={{ backdrop: Backdrop }}
-        slotProps={{
-          backdrop: {
-            TransitionComponent: Fade,
-          },
-        }}
-      >
-        <Fade in={openModal}>
-          <Box
-            sx={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              width: 400,
-              bgcolor: "background.paper",
-              boxShadow: 24,
-              p: 4,
-            }}
-          >
-            <Typography id="spring-modal-title" variant="h6" component="h2">
-              Text in a modal
-            </Typography>
-            <Typography id="spring-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
-            <div>
-              User Page
-              <button onClick={handleClick}>fetchSteamProfile</button>
-              <button onClick={handleRegister}>HANDLEREGISTER</button>
-            </div>
+    <MUIModal
+      aria-labelledby="spring-modal-title"
+      aria-describedby="spring-modal-description"
+      open={openModal}
+      onClose={handleCloseModal}
+      closeAfterTransition
+      slots={{ backdrop: Backdrop }}
+      slotProps={{
+        backdrop: {
+          TransitionComponent: Fade,
+        },
+      }}
+    >
+      <Fade in={openModal}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: 4,
+            borderRadius: "15px",
+          }}
+        >
+          <Typography variant="h6" sx={{ mb: "10px" }}>
+            Connect to your steam account
+          </Typography>
+          <Box sx={{ display: "flex" }}>
+            <TextField
+              label="SteamID"
+              variant="outlined"
+              onChange={handleChange}
+              error={error}
+              helperText={error && "No user found"}
+            />
+            <Button onClick={handlefetchSteam} sx={{ ml: "10px" }}>
+              Search
+            </Button>
           </Box>
-        </Fade>
-      </MUIModal>
-    </div>
+          <Box sx={{ mt: "15px", display:"flex", justifyContent:"space-evenly" }}>
+            {Object.keys(steamData).length === 0 ? (
+              <></>
+            ) : (
+              <>
+                <Typography variant="h6">{steamData.personaname}</Typography>
+                <Avatar alt={steamData.personaname} src={steamData.avatar} />
+              </>
+            )}
+          </Box>
+        </Box>
+      </Fade>
+    </MUIModal>
   );
 };
 
