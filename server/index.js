7117daccb8fd36cd5cced169fa37d6d5b8de813c
JakubJarosz <95305824+JakubJarosz@users.noreply.github.com>
2025-07-28 10:24:52 +0200

    ad

--- server/index.js
+++ server/index.js
@@ -19,5 +19,5 @@ app.use(express.urlencoded({extended: false}))
 
 app.use("/", routes);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
